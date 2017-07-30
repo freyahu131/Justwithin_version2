8a1cdb2c012675d2a40c28917874d636fcaa2493
mac <mac@macde-MacBook-Pro.local>
2017-07-30 14:48:12 +0800

    Initial Commit

---
+++ Justwithin_version2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Justwithin_version2
+//
+//  Created by mac on 2017/7/30.
+//  Copyright © 2017年 Justwithin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
