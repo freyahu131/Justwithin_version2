6d30753f2002064b9b4c889b13d91bc8aab9b6a4
mac <mac@macde-MacBook-Pro.local>
2017-08-20 16:28:49 +0800

    08/20 1.successful apps to judge some simple requirements 2. login and sign up（要接資料庫） 3. 字體大小問題問老師

---
+++ Justwithin_version2/ArticlesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ArticlesViewController.swift
+//  Justwithin_version2
+//
+//  Created by mac on 2017/8/20.
+//  Copyright © 2017年 Justwithin. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
